16953b1451c90c3bcfd22c0838e757de9d02083b
Dave O <davidobrite@gmail.com>
2014-06-14 18:30:28 -0700

    config.js and related specs

--- src/javascripts/config.js
+++ src/javascripts/config.js
@@ -1,13 +1,13 @@
 var process = require('process');
 
-var dev = process.env.NODE_ENV === "development";
+var dev = process.env.NODE_ENV !== "production";
 
 var config = {
   url:  (dev) ? "localhost" : "example.com",
-  port: (dev) ? 3000      : 80,
+  port: (dev) ? 3000 : 80,
   fqd: function () {
     return "http://" + this.url + ":" + this.port.toString() + "/gusher/";
-  }
+  }()
 };
 
 module.exports = config;
--- src/test/config.spec.js
+++ src/test/config.spec.js
@@ -2,11 +2,11 @@ describe('config', function () {
 
   var config;
 
-  beforeEach(function (done) {
-    config = require('config');
+  beforeEach(function () {
+    config = require('../javascripts/config');
   });
 
-  afterEach(function (done) {
+  afterEach(function () {
   });
 
   context('given when NODE_ENV is development', function () {
@@ -15,7 +15,11 @@ describe('config', function () {
     });
 
     it('constructs port according to env', function () {
-      expect(config.url).to.equal(3000);
+      expect(config.port).to.equal(3000);
+    });
+
+    it('constructs fqd according to env', function () {
+      expect(config.fqd).to.equal('http://localhost:3000/gusher/');
     });
 
   });
