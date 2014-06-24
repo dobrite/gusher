228b1ae7a5317a15ceb499081730a4f5a400ebfa
Dave O <davidobrite@gmail.com>
2014-06-23 20:57:53 -0700

    es6-promises for testing

--- src/test/integration.spec.js
+++ src/test/integration.spec.js
@@ -1,32 +1,45 @@
-var Gusher = require('../javascripts/gusher'),
-    API    = require('../javascripts/utils');
+var Promise = require('es6-promise').Promise,
+    Gusher  = require('../javascripts/gusher'),
+    API     = require('../javascripts/utils');
+
+var setupClients = function(num, done) {
+  var count = 0;
+
+  return Array.apply(null, Array(num)).map(function () {
+    var gusher = new Gusher();
+    gusher.connection.bind('connected', function () {
+      if (++count === num) done();
+    });
+    return gusher;
+  });
+};
+
+var teardownClients = function(clients, done) {
+  var count = 0,
+      clientsLength = clients.length;
+
+  return clients.map(function (gusher) {
+    gusher.connection.bind('disconnected', function () {
+      if (++count === clientsLength) done();
+    });
+    gusher.disconnect();
+  });
+};
 
 describe('Integration', function () {
 
-  var gusher1, gusher2;
+  var clients;
 
   beforeEach(function (done) {
-    gusher1 = new Gusher();
-    gusher1.connection.bind('connected', function () {
-      gusher2 = new Gusher();
-      gusher2.connection.bind('connected', function () {
-        done();
-      });
-    });
+    clients = setupClients(2, done);
   });
 
   afterEach(function (done) {
-    gusher1.connection.bind('disconnected', function () {
-      gusher2.connection.bind('disconnected', function () {
-        done();
-      });
-      gusher2.disconnect();
-    });
-    gusher1.disconnect();
+    teardownClients(clients, done);
   });
 
   it('publishes the message', function (done) {
-    var channel = gusher1.subscribe('test-channel');
+    var channel = clients[0].subscribe('test-channel');
     channel.bind('test-event', function (data) {
       expect(data.message).toEqual('yo!');
       done();
@@ -34,12 +47,20 @@ describe('Integration', function () {
     API.post('test-channel', 'test-event', { message: "yo!" });
   });
 
-  it('publishes the message to others', function (done) {
-    var channel = gusher2.subscribe('test-channel');
-    channel.bind('test-event', function (data) {
-      expect(data.message).toEqual('yo!');
+  it('publishes the message to everyone', function (done) {
+    var promises = clients.map(function (elem) {
+      return new Promise(function(resolve) {
+        elem.subscribe('test-channel').bind('test-event', resolve);
+      });
+    });
+
+    Promise.all(promises).then(function(results) {
+      results.map(function (elem) {
+        expect(elem.message).toEqual('yo!');
+      });
       done();
     });
+
     API.post('test-channel', 'test-event', { message: "yo!" });
   });
 
