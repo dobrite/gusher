cf9abe50fb27f3e111257b288b5bef4ecbe79ab9
Dave O <davidobrite@gmail.com>
2014-06-24 19:51:31 -0700

    added mutex locks to channel funcs

--- go/gusher/channel.go
+++ go/gusher/channel.go
@@ -2,9 +2,11 @@ package gusher
 
 import (
 	"gopkg.in/igm/sockjs-go.v2/sockjs"
+	"sync"
 )
 
 type channel struct {
+	mutex    sync.Mutex
 	sessions map[string]sockjs.Session
 }
 
@@ -13,19 +15,24 @@ func newChannel() *channel {
 }
 
 func (ch *channel) publish(payload string) {
+	//what happens when a session is added while looping?
 	for _, session := range ch.sessions {
 		session.Send(payload)
 	}
 }
 
 func (ch *channel) subscribe(session sockjs.Session) {
+	ch.mutex.Lock()
 	ch.sessions[session.ID()] = session
+	ch.mutex.Unlock()
 }
 
 func (ch *channel) unsubscribe(session sockjs.Session) {
 	id := session.ID()
 	_, ok := ch.sessions[id]
 	if ok {
+		ch.mutex.Lock()
 		delete(ch.sessions, id)
+		ch.mutex.Unlock()
 	}
 }
