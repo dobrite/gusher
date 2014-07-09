92ce70bcc51c7c01434ac12c2b712fc874982af1
Dave O <davidobrite@gmail.com>
2014-07-09 04:26:17 -0700

    removed flux chat example

--- src/javascripts/app.js
+++
@@ -1,4 +0,0 @@
-var React = require('react');
-var FluxChat = require('./components/FluxChat.react');
-
-React.renderComponent(FluxChat(), document.getElementById('fluxchat'));
--- src/javascripts/constants/FluxChatConstants.js
+++
@@ -1,7 +0,0 @@
-var keyMirror = require('react/lib/keyMirror');
-
-module.exports = keyMirror({
-  PUSHSTREAM_INITIALIZE: null,
-  PUSHSTREAM_CONNECT: null,
-  CHAT_SEND_MESSAGE: null
-});
