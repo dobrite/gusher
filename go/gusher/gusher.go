67fb69875ffea8b8bea14a84e13752dff909519b
Dave O <davidobrite@gmail.com>
2014-07-31 06:14:32 -0700

    removed comments

--- go/gusher/gusher.go
+++ go/gusher/gusher.go
@@ -110,14 +110,7 @@ func (h *handler) handleMessage(msg message, session *session) {
 	case messageSubscribe:
 		log.Println("subscribed to " + msg.Channel)
 		if strings.HasPrefix(msg.Channel, "private-") || strings.HasPrefix(msg.Channel, "presence-") {
-			//set authTransport to 'ajax' (default)
-			//POST to /pusher/auth w/ socket_id and channel_name
-			//set authTransport to 'jsonp', also set authEndpoint (default to /pusher/auth)
-			//JSONP to /pusher/auth w/ socket_id, channel_name and callback
-			//render :text => params[:callback] + "(" + auth.to_json + ")", :content_type => 'application/javascript'
-			//return if authorized application/json
-			//{"auth":"278d425bdf160c739803:afaed3695da2ffd16931f457e338e6c9f2921fa133ce7dac49f529792be6304c","channel_data":"{\"user_id\":10,\"user_info\":{\"name\":\"Mr. Pusher\"}}"}
-			//otherwise 403 Forbidden plain text
+			// TODO something
 		} else {
 			h.registry.subscribe(msg, session)
 			h.registry.send(session, buildMessageSubscriptionSucceeded(msg.Channel))
